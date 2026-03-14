995b7a6239ba51463dc8d6fcce366b44ab21f80f
Daniel Ronalds <nzdanron@gmail.com>
2026-03-14 19:09:37 +1300

    fix: deleting random windows

--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -1,19 +1,38 @@
 <script setup lang="ts">
-import { ref, type Ref } from 'vue';
+import { ref, type Ref } from 'vue'
 import HelloWorldApp from './apps/HelloWorldApp.vue'
 
-const HelloWorld = "helloWorld";
+const HelloWorld = 'helloWorld'
 
-type WindowType = typeof HelloWorld;
+type WindowType = typeof HelloWorld
 
-const windows: Ref<WindowType[]> = ref([]);
-
-const removeWindow = (index: number) => windows.value.splice(index, 1);
-
-const addHelloWorldWindow = () => {
-  windows.value.push(HelloWorld);
+interface WindowInstance {
+  id: string
+  type: WindowType
 }
 
+const windows: Ref<WindowInstance[]> = ref([])
+
+const removeWindow = (id: string) => {
+  const index = windows.value.findIndex((w) => w.id === id)
+
+  windows.value.splice(index, 1)
+}
+
+const generateWindowId = () => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  let randomString = ''
+
+  for (var i = 0; i < 10; i++) {
+    randomString += alphabet[Math.floor(Math.random() * alphabet.length)]
+  }
+
+  return randomString
+}
+
+const addHelloWorldWindow = () => {
+  windows.value.push({ id: generateWindowId(), type: HelloWorld })
+}
 </script>
 
 <template>
@@ -22,13 +41,20 @@ const addHelloWorldWindow = () => {
   />
 
   <nav class="flex flex-col gap-2 p-4">
-    <button class="p-2 rounded-lg bg-blue-200 shadow-md w-fit h-fit cursor-pointer" @click="addHelloWorldWindow">
+    <button
+      class="p-2 rounded-lg bg-blue-200 shadow-md w-fit h-fit cursor-pointer"
+      @click="addHelloWorldWindow"
+    >
       <p class="text-2xl">👋</p>
     </button>
   </nav>
 
-  <div v-for="(window, index) in windows">
-    <HelloWorldApp v-if="window === HelloWorld" @closed="() => removeWindow(index)"/>
+  <div v-for="window in windows">
+    <HelloWorldApp
+      v-if="window.type === HelloWorld"
+      @closed="() => removeWindow(window.id)"
+      :id="window.id"
+      :key="window.id"
+    />
   </div>
-
 </template>
--- src/components/apps/HelloWorldApp.vue
+++ src/components/apps/HelloWorldApp.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import Window from '../Window.vue'
 
+defineProps<{ id: string }>()
+
 defineEmits<{ closed: [] }>()
 
 const isMinimized = defineModel('isMinimized', { default: false })
@@ -13,8 +15,9 @@ const isMinimized = defineModel('isMinimized', { default: false })
     @closed="$emit('closed')"
     @minimized="isMinimized = true"
   >
-    <div class="flex w-full h-40 justify-center items-center">
+    <div class="flex w-full h-40 justify-center items-center flex-col">
       <p>Hello world!</p>
+      <p>{{ id }}</p>
     </div>
   </Window>
 </template>
